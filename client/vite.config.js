872c61196389ae92feb87c89e739d8d68f0fbf32
L.J Solis <l.jsolis52@yahoo.com>
2023-11-15 19:31:43 -0600

    Created file structure for our project. Also added code to the server.js, connection.js, vite.config.js., index.html, and auth.js files.

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+});
