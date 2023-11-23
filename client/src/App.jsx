e038658364f47adea4c782d158ddcd9de9f2c6d0
Kiara Johnson <kiarajohnson91@gmail.com>
2023-11-22 21:14:46 -0600

    worked on api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import React, { useState, useEffect } from 'react';
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -9,10 +10,31 @@ const client = new ApolloClient({
 });
 
 function App() {
-  return (
+
+  const [data, setData] = useState([]);
+
+ useEffect(() => {
+    fetch('https://amazon-price1.p.rapidapi.com/search?keywords=womens%20shirt&marketplace=ES', {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '309b7d3f4bmsh15d4997beee00a8p15d187jsn4055f7908903',
+        'X-RapidAPI-Host': 'amazon-price1.p.rapidapi.com'
+      }
+    })
+    .then(response => response.json())
+    .then(json => setData(json))
+    .catch(error => console.error(error));
+  }, []);
+
+  return ( 
     <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
+      <div>
+       {data.map((item) => (
+        <img key={item.ASIN} src={item.imageUrl} alt={item.title}></img>
+       ))}
+      </div>
     </ApolloProvider>
   );
 }
